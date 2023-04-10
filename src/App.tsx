1ab37a0363db07eeec813144587aa0921949f286
NycolasCruz <nycolascruzsilva@gmail.com>
2023-04-10 14:24:52 -0300

    style: small adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,26 @@
-import { useState } from 'react'
+import { HeightCalculator } from "./components/HeightCalculator"
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from "/react.svg"
+import viteLogo from "/vite.svg"
 
-import './App.scss'
-import Button from 'react-bootstrap/Button'
+import "./App.scss"
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+	return (
+		<div className="App">
+			<div>
+				<a href="https://vitejs.dev" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+				<a href="https://reactjs.org" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
 
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+			<h1>Calculadora de Altura</h1>
 
-      <h1>Vite + React</h1>
-
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+			<HeightCalculator />
+		</div>
+	)
 }
-
-export default App
