ce9ce222209f83f62e120d4ea1c8e9bd86d1d579
NycolasCruz <nycolascruzsilva@gmail.com>
2023-04-11 11:43:47 -0300

    docs: set config

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,12 @@
-import type { Preview } from "@storybook/react";
+import { themes } from "@storybook/theming";
 
-const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-  },
+export const parameters = {
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+		},
+	},
+	docs: {
+		theme: themes.dark,
+	},
 };
-
-export default preview;
