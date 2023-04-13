ebdaed818f4c541f5698ddbeb2445b5f55e79f3a
NycolasCruz <nycolascruzsilva@gmail.com>
2023-04-13 17:47:50 -0300

    style: small adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import { Logos } from "./components/Logos"
 
 export function App() {
 	return (
-		<>
+		<div id="app">
 			<Logos />
 			<h1>Calculadora de Altura</h1>
 			<HeightCalculator resultLabel="Sua altura é:" disabled />
-		</>
+		</div>
 	)
 }
