ba53e976e968cdae7254b51e83f1d5aac017a1a8
NycolasCruz <nycolascruzsilva@gmail.com>
2023-04-13 17:48:02 -0300

    docs/ new stories

---
+++ src/stories/HeightCalculator.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { HeightCalculator } from "../components/HeightCalculator";
+
+export default {
+    title: "Components/HeightCalculator",
+    component: HeightCalculator,
+} as Meta;
+
+export const Default = {} as StoryObj;
